3870fa5b90c5418faca1864cdc0cb1b6bb155aa8
Matt Joiner <anacrolix@gmail.com>
2018-05-25 20:12:50 +1000

    Improve package documentation

--- sync.go
+++ sync.go
@@ -1,10 +1,15 @@
-// Package sync records the stack when locks are taken, and when locks are
-// blocked on and exports them as pprof profiles "lockHolders" and
-// "lockBlockers". if "net/http/pprof" is imported, you can view them at
-// /debug/pprof/ on the default HTTP muxer.
+// Package sync is an extension of the stdlib "sync" package. It has extra
+// functionality that helps debug the use of synchronization primitives. The
+// package should be importable in place of "sync". The extra functionality
+// can be enabled by calling Enable() or passing a non-empty PPROF_SYNC
+// environment variable to the process.
 //
-// The API mirrors that of stdlib "sync". The package can be imported in place
-// of "sync", and is enabled by setting the envvar PPROF_SYNC non-empty.
+// Several profiles are exposed on the default HTTP muxer (and to
+// "/debug/pprof" when "net/http/pprof" is imported by the process).
+// "lockHolders" lists the stack traces of goroutines that called Mutex.Lock
+// that haven't subsequently been Unlocked. "lockBlockers" contains goroutines
+// that are waiting to obtain locks. "/debug/lockTimes" or PrintLockTimes()
+// shows the longest time a lock is held for each stack trace.
 //
 // Note that currently RWMutex is treated like a Mutex when the package is
 // enabled.
@@ -35,47 +40,46 @@ var (
 	// Those blocked on acquiring a lock.
 	lockBlockers *pprof.Profile
 
-	// Protects lockTimes.
-	lockTimesMu sync.Mutex
-	// The longest time the lock is held for any unique stack.
-	lockTimes map[[32]uintptr]time.Duration
+	// Stats on lock usage by call graph.
+	lockStatsMu      sync.Mutex
+	lockStatsByStack map[lockStackKey]lockStats
 )
 
-type lockTimesSorter struct {
-	entries []lockTime
-}
-
-func (me *lockTimesSorter) Len() int { return len(me.entries) }
-func (me *lockTimesSorter) Less(i, j int) bool {
-	return me.entries[i].held < me.entries[j].held
-}
-func (me *lockTimesSorter) Swap(i, j int) {
-	me.entries[i], me.entries[j] = me.entries[j], me.entries[i]
-}
-
-type lockTime struct {
-	stack [32]uintptr
-	held  time.Duration
-}
-
-func sortedLockTimes() []lockTime {
-	var lts lockTimesSorter
-	lockTimesMu.Lock()
-	for stack, held := range lockTimes {
-		lts.entries = append(lts.entries, lockTime{stack, held})
+type (
+	lockStats struct {
+		maxTime time.Duration
+		count   lockCount
 	}
-	lockTimesMu.Unlock()
-	sort.Sort(sort.Reverse(&lts))
-	return lts.entries
+	lockStackKey = [32]uintptr
+	lockCount    = int64
+)
+
+type stackLockStats struct {
+	stack lockStackKey
+	lockStats
 }
 
+func sortedLockTimes() (ret []stackLockStats) {
+	lockStatsMu.Lock()
+	for stack, stats := range lockStatsByStack {
+		ret = append(ret, stackLockStats{stack, stats})
+	}
+	lockStatsMu.Unlock()
+	sort.Slice(ret, func(i, j int) bool {
+		return ret[i].maxTime > ret[j].maxTime
+	})
+	return
+}
+
+// Writes out the longest time a Mutex remains locked for each stack trace
+// that locks a Mutex.
 func PrintLockTimes(w io.Writer) {
 	lockTimes := sortedLockTimes()
 	tw := tabwriter.NewWriter(w, 1, 8, 1, '\t', 0)
 	defer tw.Flush()
 	w = tw
 	for _, elem := range lockTimes {
-		fmt.Fprintf(w, "%s\n", elem.held)
+		fmt.Fprintf(w, "%s * %d\n", elem.maxTime, elem.count)
 		missinggo.WriteStack(w, elem.stack[:])
 	}
 }
@@ -86,7 +90,7 @@ func Enable() {
 	if enabled {
 		return
 	}
-	lockTimes = make(map[[32]uintptr]time.Duration)
+	lockStatsByStack = make(map[lockStackKey]lockStats)
 	lockHolders = pprof.NewProfile("lockHolders")
 	lockBlockers = pprof.NewProfile("lockBlockers")
 	http.DefaultServeMux.HandleFunc("/debug/lockTimes", func(w http.ResponseWriter, r *http.Request) {
@@ -126,14 +130,17 @@ func (m *Mutex) Lock() {
 
 func (m *Mutex) Unlock() {
 	if enabled {
-		lockHeld := time.Since(m.start)
+		d := time.Since(m.start)
 		var key [32]uintptr
 		copy(key[:], m.stack[:m.entries])
-		lockTimesMu.Lock()
-		if lockHeld > lockTimes[key] {
-			lockTimes[key] = lockHeld
+		lockStatsMu.Lock()
+		v := lockStatsByStack[key]
+		if d > v.maxTime {
+			v.maxTime = d
 		}
-		lockTimesMu.Unlock()
+		v.count++
+		lockStatsByStack[key] = v
+		lockStatsMu.Unlock()
 		lockHolders.Remove(m.hold)
 	}
 	m.mu.Unlock()
